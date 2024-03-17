0350e72930d1384010140d7300bdd3611c12e271
Hemanth <159107627+Hemanth2415@users.noreply.github.com>
2024-03-17 09:33:32 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDjMflkhHKqDF4tYd2L2uHeQyUFKeQ3ZSk",
-authDomain: "phone-auth-e2723.firebaseapp.com",
-projectId: "phone-auth-e2723",
-storageBucket: "phone-auth-e2723.appspot.com",
-messagingSenderId: "145682333623",
-appId: "1:145682333623:web:64a866f06b1ecbd37fb232"
+   //Your firebase api key;
   
 };
 
